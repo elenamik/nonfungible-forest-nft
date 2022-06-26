1139d60ee0d3283749c8bafd61f6977579f6a00f
milesthedisch <milesthedisch@gmail.com>
2022-06-26 04:44:04 -0400

    Update

--- packages/hardhat/contracts/NonFungibleForest.sol
+++ packages/hardhat/contracts/NonFungibleForest.sol
@@ -17,15 +17,15 @@ import 'hardhat/console.sol';
 contract NonFungibleForest is ERC721, Ownable {
 
     DummyBCT public BCT;
-    using Strings for uint256;
-    using HexStrings for uint160;
-    using ToColor for bytes3;
+    uint public BCTMultiplier = 2;
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     uint256 constant public _supply_cap = 10;
-    uint256 constant bct_min = 5;
+    uint256 constant bct_min = 1;
+    uint256 public _age;
+    uint256 public _requiredCarbon;
 
     // track how much BCT Bal belongs to a tokenId (id -> bct bal)
     mapping (uint256 => uint256) public tokenIdToBCTBal;
@@ -34,10 +34,18 @@ contract NonFungibleForest is ERC721, Ownable {
       BCT = DummyBCT(bctAddress);
     }
 
+    mapping (uint256 => uint32) public age;
     mapping (uint256 => bytes3) public color;
     mapping (uint256 => uint256) public chubbiness;
+    mapping (uint256 => uint256) public createdAt;
 
-
+    // decay rate -> static value
+    // height based off the carbon it holds
+    // trunk thickness based off the carbon
+    // amount of leaves based on carbon
+    // flowering, non flowering
+    // genus - oak, birch, maple, ash, pine
+    // color of the flowers is random
 
 
   function mintItem(uint256 bctAmount)
@@ -45,11 +53,16 @@ contract NonFungibleForest is ERC721, Ownable {
       returns (uint256)
   {
         uint256 id = _tokenIds.current();
+
         require(id <= _supply_cap, "NO MORE TO MINT");
         require(bctAmount >= bct_min, "BCT Deposit below the minimum");
 
+        console.log('BCT BAL', BCT.balanceOf(msg.sender));
+        console.log('BCT AMOUNT', bctAmount);
+
         // ui must have first triggered the "approve" on the BCT token
         require(BCT.balanceOf(msg.sender) >= bctAmount, 'User does not hold enough BCT');
+        console.log('ITEM ID', id);
 
         // 1. attempt the transfer of BCT to self
         bool tokenTransferSuccess = BCT.transferFrom(msg.sender, address(this), bctAmount);
@@ -61,116 +74,39 @@ contract NonFungibleForest is ERC721, Ownable {
         _mint(msg.sender, id);
         _tokenIds.increment();
 
+        // VRF for type tree
         bytes32 predictableRandom = keccak256(abi.encodePacked( blockhash(block.number-1), msg.sender, address(this), id ));
-
-        // TODO:
-          // decay rate -> static value
-          // height based off the carbon it holds
-          // trunk thickness based off the carbon
-          // amount of leaves based on carbon
-          // flowering, non flowering
-          // genus - oak, birch, maple, ash, pine
-          // color of the flowers is random
-
-      //
-      // height, based off BC and age
-      // trunk thickness, based off age
-      //
         color[id] = bytes2(predictableRandom[0]) | ( bytes2(predictableRandom[1]) >> 8 ) | ( bytes3(predictableRandom[2]) >> 16 );
         chubbiness[id] = 35+((55*uint256(uint8(predictableRandom[3])))/255);
-
+        age[id] = 0;
+        createdAt[id] = now;
+  
         return id;
   }
 
   function tokenURI(uint256 id) public view override returns (string memory) {
       require(_exists(id), "not exist");
-      string memory name = string(abi.encodePacked('Nonfungible Tree #',id.toString()));
-      string memory description = string(abi.encodePacked('This Loogie is the color #',color[id].toColor(),' with a chubbiness of ',uint2str(chubbiness[id]),'!!!'));
-      string memory image = Base64.encode(bytes(generateSVGofTokenById(id)));
 
-      return
-          string(
-              abi.encodePacked(
-                'data:application/json;base64,',
-                Base64.encode(
-                    bytes(
-                          abi.encodePacked(
-                              '{"name":"',
-                              name,
-                              '", "description":"',
-                              description,
-                              '", "external_url":"https://burnyboys.com/token/',
-                              id.toString(),
-                              '", "attributes": [{"trait_type": "color", "value": "#',
-                              color[id].toColor(),
-                              '"},{"trait_type": "chubbiness", "value": ',
-                              uint2str(chubbiness[id]),
-                              '}], "owner":"',
-                              (uint160(ownerOf(id))).toHexString(20),
-                              '", "image": "',
-                              'data:image/svg+xml;base64,',
-                              image,
-                              '"}'
-                          )
-                        )
-                    )
-              )
-          );
+      string memory name = string(abi.encodePacked('Loogie #',id.toString()));
+
+     return name;
   }
 
-  function generateSVGofTokenById(uint256 id) internal view returns (string memory) {
-
-    string memory svg = string(abi.encodePacked(
-      '<svg width="400" height="400" xmlns="http://www.w3.org/2000/svg">',
-        renderTokenById(id),
-      '</svg>'
-    ));
-
-    return svg;
+  function getAge(uint256 id) public returns (uint256) {
+    console.log(now - createdAt[id]);
+    _age = createdAt[id] - now;
   }
-
-  // Visibility is `public` to enable it being called by other contracts for composition.
-  function renderTokenById(uint256 id) public view returns (string memory) {
-    string memory render = string(abi.encodePacked(
-      '<g id="eye1">',
-          '<ellipse stroke-width="3" ry="29.5" rx="29.5" id="svg_1" cy="154.5" cx="181.5" stroke="#000" fill="#fff"/>',
-          '<ellipse ry="3.5" rx="2.5" id="svg_3" cy="154.5" cx="173.5" stroke-width="3" stroke="#000" fill="#000000"/>',
-        '</g>',
-        '<g id="head">',
-          '<ellipse fill="#',
-          color[id].toColor(),
-          '" stroke-width="3" cx="204.5" cy="211.80065" id="svg_5" rx="',
-          chubbiness[id].toString(),
-          '" ry="51.80065" stroke="#000"/>',
-        '</g>',
-        '<g id="eye2">',
-          '<ellipse stroke-width="3" ry="29.5" rx="29.5" id="svg_2" cy="168.5" cx="209.5" stroke="#000" fill="#fff"/>',
-          '<ellipse ry="3.5" rx="3" id="svg_4" cy="169.5" cx="208" stroke-width="3" fill="#000000" stroke="#000"/>',
-        '</g>'
-      ));
-
-    return render;
+  
+  function requiredCarbon(uint256 id) public returns (uint256) {
+    _requiredCarbon = age[id] * BCTMultiplier * 10000 / 1000000;
   }
+  
+  function derivedProperties() {
+    if (this.balanceOf(msg.sender) > requiredCarbon()) {
+        
+    }
 
-  function uint2str(uint _i) internal pure returns (string memory _uintAsString) {
-      if (_i == 0) {
-          return "0";
-      }
-      uint j = _i;
-      uint len;
-      while (j != 0) {
-          len++;
-          j /= 10;
-      }
-      bytes memory bstr = new bytes(len);
-      uint k = len;
-      while (_i != 0) {
-          k = k-1;
-          uint8 temp = (48 + uint8(_i - _i / 10 * 10));
-          bytes1 b1 = bytes1(temp);
-          bstr[k] = b1;
-          _i /= 10;
-      }
-      return string(bstr);
+    age * trunkAttrRand; 			
+    age * heightAttrRand;
   }
 }
